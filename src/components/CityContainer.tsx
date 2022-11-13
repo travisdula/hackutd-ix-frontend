4bdb0805a6a13ab71bbc0a6ee3069c4be01fdcf6
Travis A. Dula <travisadula@gmail.com>
2022-11-13 10:27:49 -0600

    spoofed api

--- src/components/CityContainer.tsx
+++ src/components/CityContainer.tsx
@@ -1,16 +1,21 @@
 import dallas from '../images/dallas.jpg'
 
-function CityContainer() {
+interface CityParams {
+    zipCode: string | undefined
+    city: string | undefined
+    score: number | undefined
+}
+function CityContainer(props: CityParams) {
     return (
         <div className="mx-auto flex items-center justify-center mb-6 z-10">
             <img src={dallas} className="object-contain h-32 w-32 rounded-l-lg"/>
             <div className="bg-light-container rounded-r-lg h-32 w-96 p-4">
                 <div className="font-bold text-2xl">
-                    Richardson, TX
+                    {props.city}, TX
                 </div>
-                <div>x% Similarity</div>
+                <div>{props.score}% Similarity</div>
                 <div>Greater Dallas Area</div>
-                <div>75080</div>
+                <div>{props.zipCode}</div>
             </div>
         </div>
     )
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,10 +1,14 @@
 import { Link, useParams } from 'react-router-dom'
+import { useState } from 'react'
 import waves2 from '../images/waves2.svg'
 import CityContainer from './CityContainer'
+import { send_city  }from '../drx_api'
+
 
 function Results() {
-    const {origin, destination} = useParams()
-    const results = [1, 2, 3, 4, 5,6]
+    const {ideal, destination, radius} = useParams()
+    const [cities, setCities] = useState()
+    const results = send_city(ideal, destination, +(radius || 0))
     return (
         <div className="min-h-screen bg-light-blue place-self-center flex flex-col items-center">
             <div className="bg-knock-blue w-fit mt-20 mb-2 rounded-md">
@@ -17,10 +21,11 @@ function Results() {
 
             <h1 className="italic text-[#434C5E] text-lg mb-8">find your new neighborhood</h1>
 
-            {results.map(() => (<CityContainer/>))}
+            <div>
+                {results.sort((a:any,b:any) => b[2] - a[2]).map((x: Array<any>) => (<CityContainer key={x[0]} zipCode={x[0]} city={x[1]} score={x[2]*100}/>))}
+            </div>
 
-
-            <img src={waves2} className="z-0 fixed bottom-0"/>
+            <img alt="waves" src={waves2} className="z-0 fixed bottom-0"/>
       </div>
     )
 }
