e01d7e2f0b62dc619b17dfc610207e9761a0fb44
Travis A. Dula <travisadula@gmail.com>
2022-11-13 07:30:52 -0600

    changed to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Home from './components/Home';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Results() {
+    return (<br/>);
+}
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route
+            path="/"
+            element={<Home/>}>
+        </Route>
+        <Route
+            path="/results/:origin/:destination"
+            element={<Results/>}>
+        </Route>
+      </Routes>
+    </Router>
+    
   );
 }
 
