afbed8947c1666e076c0054c21aed2b7cd36b1a5
jane luo <jxl190094@utdallas.edu>
2022-11-13 06:47:22 -0600

    moved home to separate function

---
+++ src/components/Home.tsx
@@ -0,0 +1,24 @@
+import waves1 from '../waves1.svg';
+
+function Home() {
+    return (
+        <div className="min-h-screen bg-light-blue place-self-center flex flex-col items-center">
+        <div className="bg-[#475790] w-fit mt-20 mb-2 rounded-md">
+          <div className="text-[45px] font-extrabold mx-4 text-[#EEF4FF]">
+            knock knock
+          </div>
+        </div>
+        <h1 className="italic text-[#434C5E] text-lg">
+          find your new neighborhood
+        </h1>
+        <div className="">
+          <input className="h-14 bg-[#ECEFF4] text-center mt-8 text-3xl rounded-md border-[1px] border-[#434C5E] placeholder:text-2xl" placeholder="search for metro area">
+          </input>
+          <button></button>
+        </div>
+        <img src={waves1} className="absolute inset-x-0 bottom-0"/>
+      </div>
+    )
+}
+
+export default Home
