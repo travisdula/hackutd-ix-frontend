28b5e147ab9c6556bb849919d54681100ce2c39e
Travis A. Dula <travisadula@gmail.com>
2022-11-13 11:14:33 -0600

    fixed city container input

--- src/components/CityContainer.tsx
+++ src/components/CityContainer.tsx
@@ -2,18 +2,22 @@ import dallas from '../images/dallas.jpg'
 import { Link, useParams } from 'react-router-dom'
 
 interface CityParams {
+    prior: string | undefined
     zipCode: string | undefined
     city: string | undefined
     score: number | undefined
 }
 function CityContainer(props: CityParams) {
-    const {origin, destination, zip} = useParams()
+    //const {origin, destination, zipCode} = useParams()
+    const origin = props.prior
+    const destination = props.city
+    const zipCode = props.zipCode
 
     return (
         <div className="mx-auto flex items-center justify-center mb-6 z-10">
             <img src={dallas} className="object-contain h-32 w-32 rounded-l-lg"/>
             <div className="bg-light-container rounded-r-lg h-32 w-96 p-4">
-                <Link to={`/results/:${origin}/:${destination}/:${zip}`}>
+                <Link to={`/details/:${origin}/:${destination}/:${zipCode}`}>
                     <div className="font-bold text-2xl">
                        {props.city}, TX
                     </div>
